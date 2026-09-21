4789b9bfae9a3a8fb5a133c4f644a91d25c4c91e
tsuji-tomonori <39981658+tsuji-tomonori@users.noreply.github.com>
2026-09-22 08:12:33 +0900

    Add GSM8K train 01489-01493 formalizations

---
+++ tests/lean/GSM8KSprint0922A08.lean
@@ -0,0 +1,59 @@
+import LemmaWeave.Problems.GSM8K.Sprint0922A08Models
+
+namespace LemmaWeave.Tests.GSM8KSprint0922A08
+open LemmaWeave.Problems.GSM8K.Sprint0922A08
+
+theorem jerky_needed := LemmaWeave.Problems.GSM8K.Sprint0922A08.jerky_needed
+theorem jerky_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.jerky_solution
+theorem banana_twice_as_many := LemmaWeave.Problems.GSM8K.Sprint0922A08.banana_twice_as_many
+theorem banana_difference_twice := LemmaWeave.Problems.GSM8K.Sprint0922A08.banana_difference_twice
+theorem banana_additive_total := LemmaWeave.Problems.GSM8K.Sprint0922A08.banana_additive_total
+theorem banana_difference_additive := LemmaWeave.Problems.GSM8K.Sprint0922A08.banana_difference_additive
+theorem banana_solution_both := LemmaWeave.Problems.GSM8K.Sprint0922A08.banana_solution_both
+theorem activities_game_daily := LemmaWeave.Problems.GSM8K.Sprint0922A08.activities_game_daily
+theorem activities_tv_week := LemmaWeave.Problems.GSM8K.Sprint0922A08.activities_tv_week
+theorem activities_game_week := LemmaWeave.Problems.GSM8K.Sprint0922A08.activities_game_week
+theorem activities_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.activities_solution
+theorem episodes_weekdays := LemmaWeave.Problems.GSM8K.Sprint0922A08.episodes_weekdays
+theorem episodes_weekend_daily := LemmaWeave.Problems.GSM8K.Sprint0922A08.episodes_weekend_daily
+theorem episodes_weekend := LemmaWeave.Problems.GSM8K.Sprint0922A08.episodes_weekend
+theorem episodes_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.episodes_solution
+theorem gifts_siblings := LemmaWeave.Problems.GSM8K.Sprint0922A08.gifts_siblings
+theorem gifts_parents_total := LemmaWeave.Problems.GSM8K.Sprint0922A08.gifts_parents_total
+theorem gifts_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.gifts_solution
+theorem lucy_after_spending := LemmaWeave.Problems.GSM8K.Sprint0922A08.lucy_after_spending
+theorem lucy_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.lucy_solution
+theorem balls_remaining := LemmaWeave.Problems.GSM8K.Sprint0922A08.balls_remaining
+theorem balls_relation := LemmaWeave.Problems.GSM8K.Sprint0922A08.balls_relation
+theorem balls_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.balls_solution
+theorem haircuts_earned_cycles := LemmaWeave.Problems.GSM8K.Sprint0922A08.haircuts_earned_cycles
+theorem haircuts_current_cycle := LemmaWeave.Problems.GSM8K.Sprint0922A08.haircuts_current_cycle
+theorem haircuts_paid_total := LemmaWeave.Problems.GSM8K.Sprint0922A08.haircuts_paid_total
+theorem haircuts_all_services := LemmaWeave.Problems.GSM8K.Sprint0922A08.haircuts_all_services
+theorem haircuts_solution_both := LemmaWeave.Problems.GSM8K.Sprint0922A08.haircuts_solution_both
+theorem hens_total := LemmaWeave.Problems.GSM8K.Sprint0922A08.hens_total
+theorem hens_rate_block := LemmaWeave.Problems.GSM8K.Sprint0922A08.hens_rate_block
+theorem hens_each_fifteen := LemmaWeave.Problems.GSM8K.Sprint0922A08.hens_each_fifteen
+theorem hens_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.hens_solution
+theorem earrings_monica := LemmaWeave.Problems.GSM8K.Sprint0922A08.earrings_monica
+theorem earrings_rachel := LemmaWeave.Problems.GSM8K.Sprint0922A08.earrings_rachel
+theorem earrings_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.earrings_solution
+theorem loss_seth_halves := LemmaWeave.Problems.GSM8K.Sprint0922A08.loss_seth_halves
+theorem loss_jerome_halves := LemmaWeave.Problems.GSM8K.Sprint0922A08.loss_jerome_halves
+theorem loss_veronica_halves := LemmaWeave.Problems.GSM8K.Sprint0922A08.loss_veronica_halves
+theorem loss_total_halves := LemmaWeave.Problems.GSM8K.Sprint0922A08.loss_total_halves
+theorem loss_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.loss_solution
+theorem strawberries_known := LemmaWeave.Problems.GSM8K.Sprint0922A08.strawberries_known
+theorem strawberries_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.strawberries_solution
+theorem store_sold := LemmaWeave.Problems.GSM8K.Sprint0922A08.store_sold
+theorem store_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.store_solution
+theorem shoes_riley := LemmaWeave.Problems.GSM8K.Sprint0922A08.shoes_riley
+theorem shoes_solution := LemmaWeave.Problems.GSM8K.Sprint0922A08.shoes_solution
+theorem travel_first := LemmaWeave.Problems.GSM8K.Sprint0922A08.travel_first
+theorem travel_same_speed_rest := LemmaWeave.Problems.GSM8K.Sprint0922A08.travel_same_speed_rest
+theorem travel_solution_same_speed := LemmaWeave.Problems.GSM8K.Sprint0922A08.travel_solution_same_speed
+theorem travel_slower_rest := LemmaWeave.Problems.GSM8K.Sprint0922A08.travel_slower_rest
+theorem travel_solution_slower := LemmaWeave.Problems.GSM8K.Sprint0922A08.travel_solution_slower
+theorem travel_solution_nonunique := LemmaWeave.Problems.GSM8K.Sprint0922A08.travel_solution_nonunique
+
+end LemmaWeave.Tests.GSM8KSprint0922A08

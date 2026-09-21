fb2404c60a12b197d4e5d3bd66b2565ea4c3578f
tsuji-tomonori <39981658+tsuji-tomonori@users.noreply.github.com>
2026-09-22 00:09:42 +0900

    Solve GSM8K train 01359-01363

---
+++ tests/lean/GSM8KSprint0922A00.lean
@@ -0,0 +1,57 @@
+import LemmaWeave.Problems.GSM8K.Sprint0922A00Models
+
+namespace LemmaWeave.Tests.GSM8KSprint0922A00
+open LemmaWeave.Problems.GSM8K.Sprint0922A00
+
+theorem reading_oliver := LemmaWeave.Problems.GSM8K.Sprint0922A00.reading_oliver
+theorem reading_lucy := LemmaWeave.Problems.GSM8K.Sprint0922A00.reading_lucy
+theorem reading_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.reading_solution
+theorem orchard_triple := LemmaWeave.Problems.GSM8K.Sprint0922A00.orchard_triple
+theorem orchard_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.orchard_solution
+theorem annie_burgers := LemmaWeave.Problems.GSM8K.Sprint0922A00.annie_burgers
+theorem annie_shakes := LemmaWeave.Problems.GSM8K.Sprint0922A00.annie_shakes
+theorem annie_spent := LemmaWeave.Problems.GSM8K.Sprint0922A00.annie_spent
+theorem annie_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.annie_solution
+theorem gumballs_joanna_bought := LemmaWeave.Problems.GSM8K.Sprint0922A00.gumballs_joanna_bought
+theorem gumballs_joanna_total := LemmaWeave.Problems.GSM8K.Sprint0922A00.gumballs_joanna_total
+theorem gumballs_jacques_bought := LemmaWeave.Problems.GSM8K.Sprint0922A00.gumballs_jacques_bought
+theorem gumballs_jacques_total := LemmaWeave.Problems.GSM8K.Sprint0922A00.gumballs_jacques_total
+theorem gumballs_pooled := LemmaWeave.Problems.GSM8K.Sprint0922A00.gumballs_pooled
+theorem gumballs_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.gumballs_solution
+theorem cupcake_made := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_made
+theorem cupcake_burnt := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_burnt
+theorem cupcake_eaten := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_eaten
+theorem cupcake_remaining := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_remaining
+theorem cupcake_revenue := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_revenue
+theorem cupcake_cost := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_cost
+theorem cupcake_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.cupcake_solution
+theorem college_tuition := LemmaWeave.Problems.GSM8K.Sprint0922A00.college_tuition
+theorem college_books := LemmaWeave.Problems.GSM8K.Sprint0922A00.college_books
+theorem college_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.college_solution
+theorem drive_capacity := LemmaWeave.Problems.GSM8K.Sprint0922A00.drive_capacity
+theorem drive_used := LemmaWeave.Problems.GSM8K.Sprint0922A00.drive_used
+theorem drive_remaining := LemmaWeave.Problems.GSM8K.Sprint0922A00.drive_remaining
+theorem drive_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.drive_solution
+theorem ivy_afternoon := LemmaWeave.Problems.GSM8K.Sprint0922A00.ivy_afternoon
+theorem ivy_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.ivy_solution
+theorem crowdfunding_low := LemmaWeave.Problems.GSM8K.Sprint0922A00.crowdfunding_low
+theorem crowdfunding_second := LemmaWeave.Problems.GSM8K.Sprint0922A00.crowdfunding_second
+theorem crowdfunding_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.crowdfunding_solution
+theorem heights_kim := LemmaWeave.Problems.GSM8K.Sprint0922A00.heights_kim
+theorem heights_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.heights_solution
+theorem bowling_first_equals_third := LemmaWeave.Problems.GSM8K.Sprint0922A00.bowling_first_equals_third
+theorem bowling_second := LemmaWeave.Problems.GSM8K.Sprint0922A00.bowling_second
+theorem bowling_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.bowling_solution
+theorem tape_combined := LemmaWeave.Problems.GSM8K.Sprint0922A00.tape_combined
+theorem tape_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.tape_solution
+theorem inheritance_natalie := LemmaWeave.Problems.GSM8K.Sprint0922A00.inheritance_natalie
+theorem inheritance_remaining := LemmaWeave.Problems.GSM8K.Sprint0922A00.inheritance_remaining
+theorem inheritance_rick := LemmaWeave.Problems.GSM8K.Sprint0922A00.inheritance_rick
+theorem inheritance_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.inheritance_solution
+theorem typing_micah := LemmaWeave.Problems.GSM8K.Sprint0922A00.typing_micah
+theorem typing_isaiah := LemmaWeave.Problems.GSM8K.Sprint0922A00.typing_isaiah
+theorem typing_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.typing_solution
+theorem land_people := LemmaWeave.Problems.GSM8K.Sprint0922A00.land_people
+theorem land_solution := LemmaWeave.Problems.GSM8K.Sprint0922A00.land_solution
+
+end LemmaWeave.Tests.GSM8KSprint0922A00
